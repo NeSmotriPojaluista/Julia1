1381e4883dae38cb576309c191d3d0cdc69df7f4
NeSmotriPojaluista <sergeja03@mail.ru>
2021-10-23 12:08:47 +0300

    hello1.jl

---
+++ hello1.jl
@@ -0,0 +1,20 @@
+function movemod(::Robot, side::HorizonSide)
+    while (isborder(r, side)==false)
+        move!(r, side)
+        putmarker!(r)
+    end
+end
+function goback(::Robot, side::HorizonSide)
+    while (ismarker(r)==true)
+        move!(r, side)
+    end
+end
+function hrello!(::Robot)
+    movemod(r, Nord)
+    goback(r, Sud)
+    movemod(r, Sud)
+    goback(r, Nord)
+    movemod(r, Ost)
+    goback(r, West)
+    movemod(r, West)
+    goback(r, Ost)
